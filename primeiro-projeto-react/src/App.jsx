2912a87686d3e74b7a106ce1951352d9259df54c
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2024-03-26 21:58:34 -0300

    first commit

---
+++ primeiro-projeto-react/src/App.jsx
@@ -0,0 +1,13 @@
+function App(){
+  // bloco superior
+  const nome = "Darlene"
+
+  return (
+    <div>
+      <h1>Nome do professor: {nome}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eius accusamus delectus qui corrupti aperiam eos. Quo esse similique ratione voluptas nobis, impedit modi labore in, cumque temporibus non quam?</p>
+    </div>
+  )
+}
+
+export default App
